ec56cda395cbed1a677fe35de6ecdf3ee5c54ec1
AnoraDragoon <anoradragoon@gmail.com>
2022-01-17 21:34:18 +0100

    create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HeroFormComponent } from './components/hero-form/hero-form.component';
+import { HeroesPageComponent } from './components/heroes-page/heroes-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'heroes', component: HeroesPageComponent },
+    { path: 'hero/new', component: HeroFormComponent },
+    { path: 'hero/:id/edit', component: HeroFormComponent },
+    { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
